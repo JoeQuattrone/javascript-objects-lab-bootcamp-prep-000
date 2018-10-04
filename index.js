72063012f62849174ce37540e85d9e1ac083b0b4
Joe Quattrone <joequattrone5@gmail.com>
2018-10-04 19:16:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
     return obj
 }
 
-function deleteFromObjectByKey(object, key){
-  
+function deleteFromObjectByKey(obj, key){
+  delete obj.key;
 }
