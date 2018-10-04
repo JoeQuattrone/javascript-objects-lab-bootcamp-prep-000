224f4d6a5c9522cf6b74d2401503d4df0068e6a5
Joe Quattrone <joequattrone5@gmail.com>
2018-10-04 19:10:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var obj = {prop: 1 }
 function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign( {}, obj, { [key]: value })
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+    return obj[key] = value
+}
